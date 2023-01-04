388e9e276740600e7debc632d819537e509cb077
rfmajor <rfmajor99@gmail.com>
2023-01-04 16:39:14 +0100

    Add Transactional annotation

--- src/main/java/com/example/iobackend/service/UserLoginRegisterService.java
+++ src/main/java/com/example/iobackend/service/UserLoginRegisterService.java
@@ -12,8 +12,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @AllArgsConstructor
 public class UserLoginRegisterService {
     private final UserRepository userRepository;
