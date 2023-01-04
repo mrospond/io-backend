a52215c6a91ce0d87209a4771980a8bf010c4965
rfmajor <rfmajor99@gmail.com>
2023-01-04 16:33:17 +0100

    Add JsoupConnector dependency

--- src/main/java/com/example/iobackend/service/web/CeneoItemWebScrapingService.java
+++ src/main/java/com/example/iobackend/service/web/CeneoItemWebScrapingService.java
@@ -1,14 +1,18 @@
 package com.example.iobackend.service.web;
 
 import com.example.iobackend.dto.ItemScrapingResult;
+import com.example.iobackend.jsoup.JsoupConnector;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Service
 public class CeneoItemWebScrapingService implements ItemWebScrapingService {
     private static final String URL_ROOT = "https://www.ceneo.pl/Zdrowie";
     private static final String URL_QUERY = ";szukaj-";
+    private final JsoupConnector jsoupConnector;
 
     @Override
     public List<ItemScrapingResult> findItemsByName(String name) {
