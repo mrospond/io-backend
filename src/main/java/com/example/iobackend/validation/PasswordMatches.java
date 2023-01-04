eb1e35ad8f4be5e917fdd85e79f54dd2d8231fcd
rfmajor <rfmajor99@gmail.com>
2023-01-04 11:37:44 +0100

    Update password not matching message

--- src/main/java/com/example/iobackend/validation/PasswordMatches.java
+++ src/main/java/com/example/iobackend/validation/PasswordMatches.java
@@ -15,7 +15,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Constraint(validatedBy = PasswordMatchesValidator.class)
 @Documented
 public @interface PasswordMatches {
-    String message() default "{com.example.iobackend.validation.PasswordMatches.message}";
+    String message() default "Passwords don't match";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
