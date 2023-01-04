5d252cccebf652fadf26d8a26b0a4de8aa3f55ba
rfmajor <rfmajor99@gmail.com>
2023-01-05 00:31:59 +0100

    Add support for Slf4j

--- src/main/java/com/example/iobackend/service/UserLoginRegisterService.java
+++ src/main/java/com/example/iobackend/service/UserLoginRegisterService.java
@@ -6,6 +6,7 @@ import com.example.iobackend.dto.LoginDto;
 import com.example.iobackend.dto.RegistrationDto;
 import com.example.iobackend.exceptions.UsernameAlreadyExistsException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @Transactional
 @AllArgsConstructor
 public class UserLoginRegisterService {
@@ -24,13 +26,16 @@ public class UserLoginRegisterService {
 
     public void registerNewUser(RegistrationDto registrationDto) {
         if (userRepository.existsByUsername(registrationDto.getUsername())) {
-            throw new UsernameAlreadyExistsException("Username " + registrationDto.getUsername() + " already exists");
+            String message = "Username " + registrationDto.getUsername() + " already exists";
+            log.error(message);
+            throw new UsernameAlreadyExistsException(message);
         }
         UserModel userModel = UserModel.builder()
                 .username(registrationDto.getUsername())
                 .passwordHash(passwordEncoder.encode(registrationDto.getPassword()))
                 .build();
         userRepository.save(userModel);
+        log.info("User registered: " + registrationDto.getUsername());
     }
 
     public void login(LoginDto loginDto) {
@@ -38,5 +43,6 @@ public class UserLoginRegisterService {
                 loginDto.getUsername(), loginDto.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
+        log.info("User logged in: " + loginDto.getUsername());
     }
 }
