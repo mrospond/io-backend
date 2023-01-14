4fdb730bc0ca06e0e8f8085f26f3926904ecb257
rfmajor <rfmajor99@gmail.com>
2023-01-15 00:25:52 +0100

    Refactor

--- src/main/java/com/example/iobackend/dto/ItemScrapingResult.java
+++ src/main/java/com/example/iobackend/dto/ItemScrapingResult.java
@@ -3,6 +3,7 @@ package com.example.iobackend.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -17,4 +18,5 @@ public class ItemScrapingResult {
     private List<String> reviews;
     private String productInfo;
     private String imageUrl;
+    private LocalDateTime timestamp;
 }
--- src/main/java/com/example/iobackend/mappers/ItemMapper.java
+++ src/main/java/com/example/iobackend/mappers/ItemMapper.java
@@ -4,9 +4,11 @@ import com.example.iobackend.database.entities.ItemResultModel;
 import com.example.iobackend.dto.ItemResultDto;
 import com.example.iobackend.dto.ItemScrapingResult;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface ItemMapper {
     ItemResultDto modelToDto(ItemResultModel model);
+    @Mapping(target = "url", source = "directShopUrl")
     ItemResultModel dtoToModel(ItemScrapingResult dto);
 }
