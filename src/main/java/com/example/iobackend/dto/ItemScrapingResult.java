7011a78df1cfbea0d1143a93cf8d984ae5aa8f1c
michimistrz <michi.kolek@gmail.com>
2023-01-14 17:42:38 +0100

    Testing scraping framework

--- src/main/java/com/example/iobackend/dto/ItemScrapingResult.java
+++ src/main/java/com/example/iobackend/dto/ItemScrapingResult.java
@@ -4,17 +4,24 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @Builder
 public class ItemScrapingResult {
     private String name;
-    private String url;
-    private BigDecimal price;
-    private BigDecimal shippingPrice;
-    private Long shippingDays;
-    private String currency;
-    private String shop;
+    //private String url;
+    private String price;
+    //private BigDecimal shippingPrice;
+    //private Long shippingDays;
+    private String currency="PLN";
+    private String shopName;
 
+    private String ceneoProductUrl;
+    private String directShopUrl;
+
+    private List<String> reviews;
+
+    private String productInfo;
     private String imageUrl;
 }
--- src/main/java/com/example/iobackend/service/web/CeneoItemWebScrapingService.java
+++ src/main/java/com/example/iobackend/service/web/CeneoItemWebScrapingService.java
@@ -4,6 +4,8 @@ import com.example.iobackend.dto.ItemInquiryDto;
 import com.example.iobackend.dto.ItemScrapingResult;
 import com.example.iobackend.jsoup.JsoupConnector;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -44,6 +46,22 @@ public class CeneoItemWebScrapingService implements ItemWebScrapingService {
 //        return null;
 //    }
 
+    private String constructUrlFromParameters(){return new String();}
+
+    private String findRedirectedUrl(String url) throws IOException {
+
+        Connection.Response response = Jsoup.connect(url).followRedirects(false).execute();
+
+        System.out.println(response.statusCode() + " : " + url);
+
+        if (response.hasHeader("location")) {
+            String redirectUrl = response.header("location");
+            findRedirectedUrl(redirectUrl);
+        }
+        return url;
+
+    }
+
     private List extractDataFromCeneo(ItemInquiryDto query, String url)  {
         List<ItemScrapingResult> results = new ArrayList<ItemScrapingResult>();
 
@@ -54,9 +72,30 @@ public class CeneoItemWebScrapingService implements ItemWebScrapingService {
             Element image = item.selectXpath("//div[@class='cat-prod-row__foto']//img").first();
 
             String imageUrl = image.attr("src");
-            String productName = image.attr("alt");
+            //String productName = image.attr("alt");
+            String ceneoProductUrl = item.selectXpath("//strong[@class='cat-prod-row__name']//a[contains(@class, 'go-to-product')]").attr("href");
 
-            results.add(ItemScrapingResult.builder().imageUrl(imageUrl).name(productName).build());
+            try{
+                Document productPageDocument = Jsoup.connect(ceneoProductUrl).timeout(10000).get();
+                String productName = productPageDocument.selectXpath("//div[@class='product-top__title']//h1").attr("text");
+                Element product = productPageDocument.selectXpath("//section[contains(@class, 'product-offers--standard')]////li[contains(@class, 'product-offers__list')]").first();
+                String valuePrice = product.selectXpath("//span[@class='value']").attr("text");
+                String pennyPrice = product.selectXpath("//span[@class='penny']").attr("text");
+                String price = valuePrice + "." + pennyPrice;
+                String shopName = product.selectXpath("//div[contains(@class, 'js_full-product-offer')]/div").attr("data-shopurl");
+
+                String shopRelativeUrl = product.selectXpath("//a[@class='go-to-shop']").attr("text");
+                shopRelativeUrl = "ceneo.pl" + shopRelativeUrl;
+                String shopSpecificUrl = findRedirectedUrl(shopRelativeUrl);
+
+                results.add(ItemScrapingResult.builder().imageUrl(imageUrl).name(productName).ceneoProductUrl(ceneoProductUrl)
+                                .directShopUrl(shopSpecificUrl).price(price).currency("PLN").shopName(shopName)
+                        .build());
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
         }
 
 
@@ -66,7 +105,7 @@ public class CeneoItemWebScrapingService implements ItemWebScrapingService {
     @Override
     public List<ItemScrapingResult> findItems(ItemInquiryDto query) {
         // TODO: 10.01.2023 To tylko przykładowa lista, trzeba zamienić na prawdziwe wyniki
-
+        //String queryUrl = constructUrlFromParameters(query);
         List<ItemScrapingResult> itemScrapingResults = extractDataFromCeneo(query, urlRoot);
         return itemScrapingResults;
 
